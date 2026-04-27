272e7b666d131ca842f733708f2fdf28997d0a71
Stefan Mihajlovic <stef.mihajlovicc@gmail.com>
2026-04-27 08:51:54 +0200

    Fixed an error that would appear first load of play page

--- Frontend/app/video/[videoId]/page.tsx
+++ Frontend/app/video/[videoId]/page.tsx
@@ -1,7 +1,26 @@
 "use client";
 
-import PlayPage from "~/components/playPage/playPage";
+import dynamic from "next/dynamic";
 import { FramedPage } from "../../page-shell";
+
+const PlayPage = dynamic(() => import("~/components/playPage/playPage"), {
+  ssr: false,
+  loading: () => (
+    <main className="play px-0 py-7.5">
+      <div className="flex flex-col gap-5 overflow-x-hidden">
+        <div id="playerCanvas" style={{ height: "100%" }}>
+          <div className="player-skeleton" aria-hidden="true">
+            <div className="player-skeleton__controls">
+              <span className="player-skeleton__chip player-skeleton__chip--wide"></span>
+              <span className="player-skeleton__chip"></span>
+              <span className="player-skeleton__chip player-skeleton__chip--short"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  ),
+});
 
 export default function Page() {
   return (
