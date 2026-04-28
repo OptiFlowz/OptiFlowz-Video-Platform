c6c862a6a77bf6f4bc2fe56bb3a25cecf38ff5f2
Aleksandar Radojicic <63604260+Aleksandar-Radojicic@users.noreply.github.com>
2026-04-28 11:41:28 +0200

    smol change

--- Backend/src/modules/quizzes/quiz.routes.js
+++ Backend/src/modules/quizzes/quiz.routes.js
@@ -4,15 +4,15 @@ import * as quizController from './quiz.controller.js';
 
 const router = express.Router();
 
-router.post('/create',requireAuth, quizController.createQuiz);
-router.patch('/:quizId', requireAuth, quizController.updateVideoQuiz);
-router.delete('/:quizId',requireAuth, quizController.deleteQuiz);
-router.get('/user',optionalAuth, quizController.getUserQuizzes);
+router.post('/create',requireAdmin, quizController.createQuiz);
+router.patch('/:quizId', requireAdmin, quizController.updateVideoQuiz);
+router.delete('/:quizId',requireAdmin, quizController.deleteQuiz);
+router.get('/user',requireAdmin, quizController.getUserQuizzes);
 
-router.post('/:quizId/question/create', requireAuth, quizController.createQuizQuestion);
-router.delete('/question/:questionId', requireAuth, quizController.deleteQuizQuestion);
-router.patch('/question/:questionId', requireAuth, quizController.updateQuizQuestion);
-router.get('/:quizId/questions', requireAuth, quizController.getAllQuizQuestions);
+router.post('/:quizId/question/create', requireAdmin, quizController.createQuizQuestion);
+router.delete('/question/:questionId', requireAdmin, quizController.deleteQuizQuestion);
+router.patch('/question/:questionId', requireAdmin, quizController.updateQuizQuestion);
+router.get('/:quizId/questions', requireAdmin, quizController.getAllQuizQuestions);
 
 // router.post('/:quizId/attempt/start',requireAuth, quizController.startQuizAttempt);
 // router.get('/:quizId/attempts',requireAuth, quizController.getUserQuizAttempts);
