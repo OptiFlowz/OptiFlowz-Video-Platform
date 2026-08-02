9c5d190f8a3324800e714fb3e83bb54732c078ac
Stefan Mihajlovic <stef.mihajlovicc@gmail.com>
2026-08-02 11:46:52 +0200

    Added custom scrollbars to popups

--- Frontend/app/components/quizzesPage/editQuizQuestionsPopup.tsx
+++ Frontend/app/components/quizzesPage/editQuizQuestionsPopup.tsx
@@ -446,7 +446,7 @@ function EditQuizQuestionsPopup({
             </button>
           </div>
 
-          <div className="flex min-h-0 flex-1 flex-col overflow-y-auto">
+          <div className="quizPopupScroll flex min-h-0 flex-1 flex-col overflow-y-auto">
             <div className="flex items-center gap-4">
               <h4 className="text-base font-semibold">{t("adminTableQuestions")}</h4>
             </div>
